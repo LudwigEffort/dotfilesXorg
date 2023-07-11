35f9aeaf5fcb4e508b6ce7ba69bb01adb90a1fb4
LudwigEffort <dr.feynman65@gmail.com>
2023-07-11 17:14:32 +0200

    fix bug reboot icon in rofi power menu and change notify-send to dunstify in rofi wifi menu

--- config/rofi/scripts/rofi-wifi-menu.sh
+++ config/rofi/scripts/rofi-wifi-menu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#notify-send "Getting list of available Wi-Fi networks..."
+dunstify -i "/home/ludwig/.local/share/icons/fontawesome/wifi.svg" "List of available Wi-Fi..."
 # Get a list of available wifi connections and morph it into a nice-looking list
 wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
 
@@ -18,21 +20,20 @@ chosen_id=$(echo "${chosen_network:3}" | xargs)
 if [ "$chosen_network" = "" ]; then
 	exit
 elif [ "$chosen_network" = "󰖩  Enable Wi-Fi" ]; then
-	nmcli radio wifi on
+	nmcli radio wifi on | dunstify -i "/home/ludwig/.local/share/icons/fontawesome/wifi.svg" "Wi-Fi Enabled"
 elif [ "$chosen_network" = "󰖪  Disable Wi-Fi" ]; then
-	nmcli radio wifi off
+	nmcli radio wifi off | dunstify -i "/home/ludwig/.local/share/icons/fontawesome/wifi.svg" "Wi-Fi Disabled"
 else
 	# Message to show when connection is activated successfully
-	#success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
+	success_message="You are now connected to \"$chosen_id\"."
 	# Get saved connections
 	saved_connections=$(nmcli -g NAME connection)
 	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully"
+		nmcli connection up id "$chosen_id" | grep "successfully" && dunstify -i "/home/ludwig/.local/share/icons/fontawesome/wifi.svg" "Connection Established" "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
 			wifi_password=$(rofi -dmenu -p "Password: " )
 		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully"
+		nmcli d wifi connect "$chosen_id" password "$wifi_password" | grep "successfully"
 	fi
 fi
-
